94458edc627dedb6112beea2e37bf6ec3739637b
Katherine Ordoñez <kmarienn02@gmail.com>
2023-06-08 13:35:09 -0500

    Acttualizado. CLASE CURSO

--- sap/personas/admin.py
+++ sap/personas/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from personas.models import Persona
+from personas.models import Persona, Curso
+
 
 # Register your models here.
 admin.site.register(Persona)
+admin.site.register(Curso)
--- sap/personas/models.py
+++ sap/personas/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=50,null = True)
+    horas = models.IntegerField(null = True)
+    profesor = models.CharField(max_length=100,null = True)
+
+    def __str__(self):
+        return f'{self.nombre}'
+
 class Persona(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     activo = models.BooleanField(default=True)
+    curso = models.ForeignKey(Curso, on_delete = models.SET_NULL, null = True)
 
     def __str__(self):
         return f'id: {self.id} -{self.nombre} {self.apellido}'
