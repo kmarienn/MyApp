548db27cb7f792103ac96917a62bb3826a8ad8a3
Katherine Ordoñez <kmarienn02@gmail.com>
2023-06-07 23:09:54 -0500

    codigos

--- sap/sap/urls.py
+++ sap/sap/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', bienvenida),
+    path('hola/<str:nombre>', hola),
+    path('edad/<int:edad>', edad),
+    path('personas', registro),
+
 ]
